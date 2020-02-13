5ac3e16ecff125d1748bb28e10dd0744a885638f
nickcanfield29 <nickcanfield29@gmail.com>
2020-02-14 01:25:05 +0700

    Update BlackJack.py

--- BlackJack.py
+++ BlackJack.py
@@ -1,22 +1,22 @@
 import random
 
 def dealer_draw():
-    cards = []
+    dealer_cards = []
     initial_card = random.randrange(2, 12)
     second_initial_card = random.randrange(2, 12)
-    cards.append(initial_card)
-    cards.append(second_initial_card)
+    dealer_cards.append(initial_card)
+    dealer_cards.append(second_initial_card)
     exit = False
     while exit == False:
         random_card = random.randrange(2, 12)
-        if random_card == 11 and (sum(cards) + 11) > 21:
+        if random_card == 11 and (sum(dealer_cards) + 11) > 21:
             random_card = 1
-        cards.append(random_card)
-        if sum(cards) < 17:
+        dealer_cards.append(random_card)
+        if sum(dealer_cards) < 17:
             continue
-        elif sum(cards) >= 17:
+        elif sum(dealer_cards) >= 17:
             exit = True
-    return sum(cards)
+    return dealer_cards
 
 def print_score(win_count, lose_count):
     print("Games Won: ", win_count)
@@ -28,61 +28,85 @@ def blackjack():
     exit = False
     while exit == False:
         cards = []
-        y = input("Want to play?\n")
+        if win_count == 0 and lose_count == 0:
+          print('Want to play?')
+        if win_count != 0 or lose_count != 0:
+          print('Want to play again?')
+        print('Type y for yes')
+        print('Type n for no')
+        y = input()
+        print('\n' * 2)
         if y.lower() in ['yes', 'y']:
             initial_card = random.randrange(2, 12)
             second_initial_card = random.randrange(2, 12)
             cards.append(initial_card)
             cards.append(second_initial_card)
+            dealer_cards = dealer_draw()
             exit2 = False
             while exit2 == False:
+                print('\n' * 100)
                 print("Your cards value: ", sum(cards))
-                print(cards)
-                x = input("Want to hit?\n")
+                if sum(cards) == 21:
+                  print("BlackJack!")
+                print("Your cards: ", cards)
+                print("Dealer's Showing Card: ", dealer_cards[0])
+                print('\n' * 2)
+                print('Want to hit?')
+                print('Type y for yes')
+                print('Type n for no')
+                x = input()
+                print('\n' * 2)
                 if x.lower() in ['yes', 'y']:
+                    print('\n' * 100)
                     random_card = random.randrange(2, 12)
                     if random_card == 11 and (sum(cards) + 11) > 21:
                         random_card = 1
                     cards.append(random_card)
                     if sum(cards) == 21:
                         print("You got blackjack!")
+                        print('\n' * 2)
                         print("Your cards: ", cards)
-                        dealer_value = dealer_draw()
-                        print("Dealer got: ", dealer_value)
-                        if dealer_value > 21:
+                        print("Dealer got: ", dealer_cards)
+                        if sum(dealer_cards) > 21:
                             print("Dealer Busted. You win!")
                             win_count += 1
-                        if dealer_value >= sum(cards) and dealer_value < 21:
+                        if sum(dealer_cards) >= sum(cards) and sum(dealer_cards) <= 21:
                             print("You lose.")
                             lose_count += 1
-                        if dealer_value < sum(cards):
+                        if sum(dealer_cards) < sum(cards):
                             print("You win!")
                             win_count += 1
+                        print('\n' * 2)
                         print_score(win_count, lose_count)
+                        print('\n' * 2)
                         exit2 = True
                     elif sum(cards) > 21:
                         print("Bust!")
                         print("Your final cards value: ", sum(cards))
                         lose_count += 1
-                        print("Thanks for playing!")
+                        print('\n' * 2)
                         print_score(win_count, lose_count)
                         exit2 = True
+                        print('\n' * 2)
                 else:
+                    print('\n' * 100)
                     print("Your final cards value: ", sum(cards))
-                    dealer_value = dealer_draw()
-                    print("Dealer got: ", dealer_value)
-                    if dealer_value > 21:
+                    print("Dealer got: ", sum(dealer_cards))
+                    if sum(dealer_cards) > 21:
                         print("Dealer Busted. You win!")
                         win_count += 1
-                    if dealer_value >= sum(cards) and dealer_value < 21:
+                    if sum(dealer_cards) >= sum(cards) and sum(dealer_cards) <= 21:
                         print("You lose.")
                         lose_count += 1
-                    if dealer_value < sum(cards):
+                    if sum(dealer_cards) < sum(cards):
                         print("You win!")
                         win_count += 1
+                    print('\n' * 2)
                     print_score(win_count, lose_count)
                     exit2 = True
+                    print('\n' * 2)
         else:
+            print('\n' * 100)
             print("Thanks for playing!")
             print_score(win_count, lose_count)
             exit = True
